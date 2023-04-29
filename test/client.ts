39ad6a170257084304760d950fef7107647f1bc7
kjoedicker <kjoedicker@merkleinc.com>
2023-04-29 12:38:52 -0400

    test(client): add testing around the `delete` key method

--- test/client.ts
+++ test/client.ts
@@ -53,7 +53,7 @@ describe("RustKeyClient", function() {
     describe('get()', function() {
         
         context('The key does not exist', function() {
-            it('should return', async function() {
+            it('should return `404` indicating the key does not exist', async function() {
                 const 
                     response = await Client.get("notARealKey"),
                     { status, data } = response
@@ -88,5 +88,47 @@ describe("RustKeyClient", function() {
         }
     });
 
+    describe('delete()', function() {
+        
+        for (const {key, value } of scenarios ) {
 
-})
+            context(`delete() is called with the key \`${key}\` which has a value of \`${value}\``, function() {
+
+                before(() => Client.set(key,value));
+                
+                it('should return `200` indicating the delete was successful', async function () {
+                    const 
+                        response = await Client.delete(key),
+                        { status, data } = response
+                    ;
+
+                    expect(status).to.equal(200);
+                });
+
+                context('get() is called on the deleted key', function() {
+                    it('should return a `404 indicating the key does not exist`', async function() {
+                        const
+                            response = await Client.get(key),
+                            { status, data } = response
+                        ;
+                    
+                        expect(status).to.equal(404);
+                        expect(data).to.match(/Key not found/);
+                    });
+                });
+            });
+        }
+
+        context('delete() is  called on a key that does not exist', function() {
+            it('should return `404` indicating that the key does not exist', async function() {
+                const 
+                    response = await Client.delete("imNotRealINeverWas"),
+                    { status, data } = response
+                ;
+
+                expect(status).to.equal(404);
+                expect(data).to.match(/Key not found/);
+            });
+        });
+    });
+});
